a4a03b3e7612a32a66e75ce82a266a064694e6f8
BLVCK7 <101505525+BLVCK7@users.noreply.github.com>
2022-04-18 15:09:56 +0300

    v.6

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import axios from 'axios';
+import { setPizzas as setPizzasAction } from './redux/actions/pizzas';
+import { connect } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
-function App() {
-  const [pizzas, setPizzas] = React.useState([]);
+// function App() {
+//   React.useEffect(() => {
+// axios.get('http://localhost:3000/db.json').then(({ data }) => {
+//   setPizzas(data.pizzas);
+//     });
+//   }, []);
 
-  React.useEffect(() => {
+class App extends React.Component {
+  componentDidMount() {
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      setPizzas(data.pizzas);
+      this.props.setPizzas(data.pizzas);
     });
-  }, []);
+  }
 
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home items={pizzas} />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home items={this.props.items} />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    items: state.pizzas.items,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setPizzas: (items) => dispatch(setPizzasAction(items)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 // Использование без библиотеки Axios (обычным джава скриптом)
 // fetch('http://localhost:3000/db.json')
