b4642baf9bedf60e46c96f7d0b0d000842fbcb3e
BLVCK7 <101505525+BLVCK7@users.noreply.github.com>
2022-04-19 16:17:43 +0300

    v.7

--- src/App.js
+++ src/App.js
@@ -1,54 +1,50 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import axios from 'axios';
-import { setPizzas as setPizzasAction } from './redux/actions/pizzas';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
+import { setPizzas } from './redux/actions/pizzas';
 
-// function App() {
-//   React.useEffect(() => {
-// axios.get('http://localhost:3000/db.json').then(({ data }) => {
-//   setPizzas(data.pizzas);
-//     });
-//   }, []);
+function App() {
+  const dispatch = useDispatch();
 
-class App extends React.Component {
-  componentDidMount() {
-    axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      this.props.setPizzas(data.pizzas);
+  React.useEffect(() => {
+    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+      dispatch(setPizzas(data));
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home items={this.props.items} />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </div>
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-  };
-};
+export default App;
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setPizzas: (items) => dispatch(setPizzasAction(items)),
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.pizzas.items,
+//     filters: state.filters,
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     setPizzas: (items) => dispatch(setPizzasAction(items)),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 // Использование без библиотеки Axios (обычным джава скриптом)
 // fetch('http://localhost:3000/db.json')
